a05cc1b081e8f55776fe6f73ba572cf07d8d222a
Yein kim <skdla10081@gmail.com>
2020-08-17 14:49:11 +0900

    턴으로 묶기 전에 마지막..

--- warr.js
+++ warr.js
@@ -1,68 +1,110 @@
-var my = {
-    ATK: 3,
-    DEF: 3,
-    HP: 10
+function defendvalue(defender) {
+    return Math.ceil(Math.random() * defender.DEF);
 }
+
+var my = {
+    id: 'my',
+    name: '와르르엄마',
+    HP: 10,
+    HPElement: document.getElementById('my-hp'),
+    ATK: 3,
+    ATKElement: document.getElementById('my-atk'),
+    DEF: 3,
+    DEFElement: document.getElementById('my-def'),
+    character: document.getElementById('me'),
+    attackedMotion: document.getElementById('me-attacked'),
+    damageValue: document.getElementById('me-damage'),
+}
+
+my.ATKElement.textContent = 'ATK : ' + my.ATK;
+my.DEFElement.textContent = 'DEF : ' + my.DEF;
 
 var mold = {
-    ATK: 3,
-    DEF: 3,
+    id: 'mold',
+    name: '곰팡이',
     HP: 10,
+    HPElement: document.getElementById('mold-hp'),
+    ATK: 3,
+    ATKElement: document.getElementById('mold-atk'),
+    DEF: 3,
+    DEFElement: document.getElementById('mold-def'),
+    character: document.getElementById('mold'),
+    attackedMotion: document.getElementById('mold-attacked'),
+    damageValue: document.getElementById('mold-damage'),
 }
 
-function attack(attacker, defender) {
-    defender.HP = defender.HP - (attacker.ATK - defender.DEF);
-    defender.HPElement.textContent = 'HP : ' + defender.HP;
-}
+mold.ATKElement.textContent = 'ATK : ' + mold.ATK;
+mold.DEFElement.textContent = 'DEF : ' + mold.DEF;
 
 
-///my
-//my.HPElement = document.getElementById('my-hp');
-var myHP = document.getElementById('my-hp');
-
-var myATK = document.getElementById('my-atk');
-myATK.textContent = 'ATK : ' + my.ATK;
-
-var myDEF = document.getElementById('my-def');
-myDEF.textContent = 'DEF : ' + my.DEF;
-
-
-///mold
-var moldHP = document.getElementById('mold-hp');
-
-var moldATK = document.getElementById('mold-atk');
-moldATK.textContent = 'ATK : ' + mold.ATK;
-
-var moldDEF = document.getElementById('mold-def');
-moldDEF.textContent = 'DEF : ' + mold.DEF;
-
-
-//def
-var mold_def = Math.ceil(Math.random() * mold.DEF);
-var my_def = Math.ceil(Math.random() * my.DEF);
-
-
+//animation
 var attack_block = document.getElementById('attack-block');
-attack_block.classList.add('display-none')
 
 var attackbtn = document.getElementById('attack');
 
-var moldcharacter = document.getElementById('mold');
-var mycharacter = document.getElementById('me');
+//animation-setting
+my.attackedMotion.classList.add('display-none');
+mold.attackedMotion.classList.add('display-none');
 
-var me_attacked = document.getElementById('me-attacked')
-var mold_attacked = document.getElementById('mold-attacked')
-me_attacked.classList.add('display-none')
-mold_attacked.classList.add('display-none')
+// var mold_damage = document.getElementById('mold-damage')
+// var me_damage = document.getElementById('me-damage')
 
-var mold_damage = document.getElementById('mold-damage')
-var me_damage = document.getElementById('me-damage')
+
+function attack(attacker, defender) {
+    defender.defendvalue = defendvalue(defender);
+    defender.HP = defender.HP - Math.max(0, attacker.ATK - defender.defendvalue);
+}
 
 function reset() {
     my.HP = 10;
     mold.HP = 10;
-    myHP.textContent = 'HP : ' + my.HP;
-    moldHP.textContent = 'HP : ' + mold.HP;
+    my.HPElement.textContent = 'HP : ' + my.HP;
+    mold.HPElement.textContent = 'HP : ' + mold.HP;
+    attackbtn.disabled = false;
+    attack_block.classList.add('display-none');
+    attack_block.classList.remove('attack-mold');
+    attack_block.classList.remove('attack-my');
+    my.character.classList.add('jumping');
+    mold.character.classList.remove('jumping');
+    my.damageValue.textContent = '';
+    mold.damageValue.textContent = '';
+}
+
+function gameEnd(winner) {
+    alert(String(winner.name) + ' 승!');
+    reset();
+}
+
+function attackedAnimation(attacker, defender) {
+    if (attacker.ATK - defender.defendvalue > 0) {
+        defender.attackedMotion.classList.remove('display-none');
+        setTimeout(function () {
+            defender.attackedMotion.classList.add('display-none');
+        }, 500);
+    } else { }
+}
+
+function turn(attacker, defender) {
+    attack(attacker, defender);
+    attackedAnimation(attacker, defender);
+
+    attack_block.classList.remove('attack-' + attacker.id);
+    attack_block.classList.add('display-none');
+
+    defender.damageValue.textContent = '-' + Math.max(0, attacker.ATK - defender.defendvalue);
+    //남은 HP 반영
+    defender.HPElement.textContent = 'HP : ' + defender.HP;
+}
+
+function waiting_attack_animation(attacker, defender) {
+    attacker.damageValue.textContent = ' ';
+    defender.character.classList.remove('jumping');
+    attacker.character.classList.add('jumping');
+}
+
+function block_attack_animation(attacker) {
+    attack_block.classList.remove('display-none');
+    attack_block.classList.add('attack-' + attacker.id);
 }
 
 reset();
@@ -72,81 +114,31 @@ reset();
 //-------------------------------------------------
 
 
-
-mycharacter.classList.add('jumping');
-
 attackbtn.addEventListener('click', function (e) {
     e.preventDefault();
-    
-    //내 턴!-----------
     attackbtn.disabled = true;
-    attack_block.classList.remove('display-none');
-    attack_block.classList.add('attack-me')
+    block_attack_animation(my);
     setTimeout(function () {
-        attack_block.classList.add('display-none');
-
-        //공격
-        mold.HP = mold.HP - (my.ATK - mold_def);
-        if (my.ATK - mold_def > 0) {
-            mold_attacked.classList.remove('display-none');
-            setTimeout(function () {
-                mold_attacked.classList.add('display-none');
-            }, 500)
+        turn(my, mold);
+        if (mold.HP < 1) {
+            gameEnd(my);
         } else {
-        }
-        attack_block.classList.remove('attack-me')
-        mold_damage.textContent = '-' + (my.ATK - mold_def);
-        setTimeout(function () {
-            mold_damage.textContent = ' ';
-        }, 500)
-        //남은 HP 반영
-        moldHP.textContent = 'HP : ' + mold.HP;
-
-        setTimeout(function () {
-            //곰팡이 공격 애니메이션
-            mold_def = Math.floor(Math.random() * 3) + 1;
-            mycharacter.classList.remove('jumping');
-            moldcharacter.classList.add('jumping');
-            attack_block.classList.remove('display-none');
-            attack_block.classList.add('attack-mold')
-
-            if (mold.HP < 1) {
-                alert('와르르 엄마 승!')
-                reset()
-                attackbtn.disabled = false;
-            } else {
+            setTimeout(function () {
+                waiting_attack_animation(mold, my)
+                block_attack_animation(mold);
                 setTimeout(function () {
-                    // 내가 맞았을때 애니메이션
-                    if (mold.ATK - my_def > 0) {
-                        me_attacked.classList.remove('display-none');
-                        setTimeout(function () {
-                            me_attacked.classList.add('display-none');
-                            attack_block.classList.remove('attack-mold');
-                        }, 500)
+                    turn(mold, my);
+                    if (my.HP < 1) {
+                        gameEnd(mold);
                     } else {
+                        setTimeout(function () {
+                            waiting_attack_animation(my, mold)
+                            //attackbtn.disabled = false;
+                        }, 500);
                     }
-
-                    // 곰팡이 공격 정산-----------
-                    attack_block.classList.remove('attack-mold')
-                    attack_block.classList.add('display-none');
-
                     attackbtn.disabled = false;
-                    my.HP = my.HP - (mold.ATK - my_def);
-                    myHP.textContent = 'HP : ' + my.HP;
-                    me_damage.textContent = '-' + (mold.ATK - my_def);
-                    my_def = Math.floor(Math.random() * my.DEF) + 1;
-
-                    setTimeout(function () {
-                        me_damage.textContent = '';
-                        moldcharacter.classList.remove('jumping');
-                        mycharacter.classList.add('jumping');
-                        if (my.HP < 1) {
-                            alert('곰팡이 승!')
-                            reset()
-                        }
-                    }, 500)
-                }, 2000)
-            }
-        }, 500)
-    }, 2000)
-})
+                }, 2000);
+            }, 500);
+        }
+    }, 2000);
+});
